250a8581e12d42bb96b809c868d7898e0b443644
nbtm-sh <dog@nbtm.sh>
2026-03-11 02:23:12 +1100

    fix(url): fix syntax error

--- szurubooru_api/url.py
+++ szurubooru_api/url.py
@@ -2,6 +2,6 @@ def get_opts(**kwargs):
     # TODO: Escape strings
     output = []
     for key in kwargs.keys():
-        output.append("=".join(key, str(kwargs[key])))
+        output.append("=".join([key, str(kwargs[key])]))
     re = "&".join(output)
     return "?" + re
